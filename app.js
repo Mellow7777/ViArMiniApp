e82c4302d116c1cbd54ded8719c94432181e3fbc
Mellow7777 <145897677+Mellow7777@users.noreply.github.com>
2026-08-27 01:46:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let products = [];
 let shops = [];
 
 const API_BASE_URL =
-    "https://api.via-r-order.com";
+    "https://api-dev.via-r-order.com";
     
 const ADMIN_TELEGRAM_IDS = [
     564525966
